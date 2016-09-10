7b868313b5c4abfb3def966c67092cce9ee59b3a
p4vlos <nicolaou_paul@hotmail.com>
2016-09-10 18:53:56 +0300

    need to compare current location coordinates with region

--- Drinkable/ViewController.swift
+++ Drinkable/ViewController.swift
@@ -48,22 +48,44 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     
     func centerMapOnLocation(location: CLLocation) {
         let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, 2000, 2000)
-        
         mapView.setRegion(coordinateRegion, animated: true)
     }
 
 
     func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
-        
         if let loc = userLocation.location {
             if !mapHasCenteredOnce {
                 centerMapOnLocation(location: loc)
                 mapHasCenteredOnce = true
             }
-            
         }
     }
     
+    
+    func createRegion(location: CLLocation) {
+        //My Coordinates: 35.171296, 33.361021
+        
+        // Coordinates
+        //we will change the data from google firebase data
+        let Lat:CLLocationDegrees = 27.175015
+        let Long:CLLocationDegrees = 78.042139
+        
+        let coordinate = CLLocationCoordinate2D(latitude: Lat, longitude: Long)
+        
+        //Span
+        let latDelta:CLLocationDegrees = 0.1
+        let longDelta:CLLocationDegrees = 0.1
+        let theSpan = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: longDelta)
+        
+        let theRegion = MKCoordinateRegion(center: coordinate, span: theSpan)
+
+        
+        
+//        if theRegion == location.coordinate {
+//            print("The water is clean")
+//        }
+
+    }
     
 }
 
